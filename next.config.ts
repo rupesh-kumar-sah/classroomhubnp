4d6267a415279a1f1ee91bca377c523172ae33c5
Rupesh sah <rsah0123456@gmail.com>
2025-10-02 07:07:43 +0000

    🔹 Steps to deploy Next.js app on GitHub Pages
    Update next.config.js
    Ad

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type {NextConfig} from 'next';
 
-const isProd = process.env.NODE_ENV === 'production';
 const repoName = 'classroomhubnp';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? `/${repoName}` : '',
-  assetPrefix: isProd ? `/${repoName}/` : '',
+  basePath: `/${repoName}`,
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
