957c3cb946256ceec127689069e3003e2b1261a3
Rupesh sah <rsah0123456@gmail.com>
2025-10-02 07:01:57 +0000

    jhjk

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type {NextConfig} from 'next';
 
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'classroomhubnp';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
